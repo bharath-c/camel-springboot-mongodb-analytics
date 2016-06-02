99a3ac90fecc794b4374427bf1237113762dae25
Subramanian, Singaram <to.ramsubramanian@gmail.com>
2016-06-02 13:35:26 -0700

    camel-mongodb automatically does type conversion from String to DBObject. See @org.apache.camel.component.mongodb.converters.MongoDbBasicConverters:fromStringToDBObject() for more info.

--- src/main/java/org/zetrahytes/app/router/TopNGroupsDisplayRoute.java
+++ src/main/java/org/zetrahytes/app/router/TopNGroupsDisplayRoute.java
@@ -6,8 +6,6 @@ import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
 import com.mongodb.BasicDBList;
-import com.mongodb.DBObject;
-import com.mongodb.util.JSON;
 
 @Component
 public class TopNGroupsDisplayRoute extends RouteBuilder {
@@ -20,17 +18,12 @@ public class TopNGroupsDisplayRoute extends RouteBuilder {
             { $limit : 10 }
         ])
      */
+    String aggregateQuery = "[{ $match : { response : \"yes\" } }, { $group : { _id : \"$group.group_name\", count : {$sum : 1}} }, { $sort : {count: -1} }, { $limit : 10 }]";
+    
     @Override
     public void configure() throws Exception {
         from("timer://rsvpsaggregator?fixedRate=true&period=10000")
-            .setBody().constant("[{ $match : { response : \"yes\" } }, { $group : { _id : \"$group.group_name\", count : {$sum : 1}} }, { $sort : {count: -1} }, { $limit : 10 }]")
-            .process(new Processor() { // transform the query to something MongoDB understands
-                @Override
-                public void process(Exchange exchange) throws Exception {
-                    String payload = exchange.getIn().getBody(String.class);
-                    exchange.getIn().setBody((DBObject) JSON.parse(payload));
-                }
-            })
+            .setBody().constant(aggregateQuery)
             .to("mongodb:mongoClient?database=meetup&collection=rsvps&operation=aggregate")
             .process(new Processor() { // read the result from MongoDB
                 @Override
