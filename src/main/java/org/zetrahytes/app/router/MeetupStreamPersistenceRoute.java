c14da7bdf6ced8133862013258bbcaa271c27284
Subramanian, Singaram <to.ramsubramanian@gmail.com>
2016-06-01 23:28:38 -0700

    added a message broker, rabbitmq, between web socket client and mongodb. Here's the modified flow - route 1: receives the json notifications from meetup web socket stream and sends it to rabbitmq; route2: consumes json from rabbitmq and saves it in mongodb

---
+++ src/main/java/org/zetrahytes/app/router/MeetupStreamPersistenceRoute.java
@@ -0,0 +1,16 @@
+package org.zetrahytes.app.router;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MeetupStreamPersistenceRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("rabbitmq://localhost/meetupExchange?queue=rsvps")
+            .convertBodyTo(String.class)
+            .to("mongodb:mongoClient?database=meetup&collection=rsvps&operation=insert");
+    }
+    
+}
--- src/main/java/org/zetrahytes/app/router/MeetupStreamRouter.java
+++ src/main/java/org/zetrahytes/app/router/MeetupStreamRouter.java
@@ -9,7 +9,7 @@ public class MeetupStreamRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("ahc-ws://stream.meetup.com/2/rsvps")
-            .to("mongodb:mongoClient?database=meetup&collection=rsvps&operation=insert");
+            .to("rabbitmq://localhost/meetupExchange?queue=rsvps");
     }
 
 }
