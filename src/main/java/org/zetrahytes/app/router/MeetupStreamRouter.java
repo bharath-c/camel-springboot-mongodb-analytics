781978458ea2358851a421c030a179fb599b65ba
Subramanian, Singaram <to.ramsubramanian@gmail.com>
2016-06-02 16:48:38 -0700

    added a filter to capture only accepted invites from meetup stream for analysis; an example for Message Filter EIP @ http://camel.apache.org/message-filter.html

--- src/main/java/org/zetrahytes/app/router/MeetupStreamRouter.java
+++ src/main/java/org/zetrahytes/app/router/MeetupStreamRouter.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MeetupStreamRouter extends RouteBuilder {
-
+    
+    // an example for Message Filter pattern (See http://camel.apache.org/message-filter.html)
     @Override
     public void configure() throws Exception {
         from("ahc-ws://stream.meetup.com/2/rsvps")
+            .filter().jsonpath("$[?(@.response == 'yes')]") // only accepted invites for now
             .to("rabbitmq://localhost/meetupExchange?queue=rsvps");
     }
 
