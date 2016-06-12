cf982f2f1a38e1e96e2306a8a451f372b7a703aa
Subramanian, Singaram <to.ramsubramanian@gmail.com>
2016-06-11 22:38:47 -0700

    Meetup RSVPs are read from RabbitMQ and forwarded to Elasticsearch in addition to MongoDB. Why both? Just trying out to see if Kibana dashboard can be built using the data indexed by Elasticsearch

--- src/main/java/org/zetrahytes/app/route/MeetupStreamPersistenceRoute.java
+++ src/main/java/org/zetrahytes/app/route/MeetupStreamPersistenceRoute.java
@@ -5,12 +5,24 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MeetupStreamPersistenceRoute extends RouteBuilder {
-
+    
+    /* 
+     * Meetup RSVPs are read from RabbitMQ and forwarded to both MongoDB and Elasticsearch. 
+     * Why both? Just trying out to see if Kibana dashboard can be built using the data indexed by Elasticsearch
+     * 
+     * camel-elasticsearch: Configured as a transport client that connects to a locally running elasticsearch
+     * cluster named 'my-elasticsearch-cluster' using the transport ip (127.0.0.1) and port (9300).
+     * 9300 is the default port, so it's skipped in the URI.
+     * (See https://www.elastic.co/guide/en/elasticsearch/client/java-api/current/transport-client.html)
+     */
     @Override
     public void configure() throws Exception {
         from("rabbitmq://localhost/meetupExchange?queue=rsvps")
             .convertBodyTo(String.class)
-            .to("mongodb:mongoClient?database=meetup&collection=rsvps&operation=insert");
+            .multicast().to(
+                "mongodb:mongoClient?database=meetup&collection=rsvps&operation=insert",
+                "elasticsearch://my-elasticsearch-cluster?operation=INDEX&indexName=meetup&indexType=rsvps&ip=127.0.0.1"
+             );
     }
     
 }
